ad154941758d6a1545f003410f99a7325e59b126
SlavaPachevsky <savaukr17101980@gmail.com>
2022-05-21 18:30:08 +0300

    add config

---
+++ cmd/server/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"github.com/savaukr/restApiGinGorm/pkg/config"
+	"uber-go.org/zap"
+)
+
+func main() {
+	prdLogger, _ := zap.NewProduction()
+	defer prdLogger.Sync()
+	logger := prdLogger.Sugar()
+
+	cfg, err != config.NewConfig()
+	if err nil {
+		logger.Fatalw("failed to parse config", "err", err)
+	}
+	fmt.Printf("cfg = %+v\n", cfg)
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/savaukr/restApiGinGorm
+
+go 1.18
+
+require github.com/kelseyhightower/envconfig v1.4.0 // indirect
